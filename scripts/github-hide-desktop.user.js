b0e44ef32896660640352845a24512579b29984f
gaojr <gaojr427@hotmail.com>
2019-12-02 18:27:01 +0800

    update script
    * using library

--- scripts/github-hide-desktop.user.js
+++ scripts/github-hide-desktop.user.js
@@ -3,22 +3,23 @@
 // @namespace https://github.com/gaojr/tampermonkey-scripts
 // @name:CN-zh_cn GitHub隐藏Desktop按钮
 // @name GitHubHideDesktop
-// @version 0.1
+// @version 0.2
 // @description hide "Open in GitHub Desktop" button
 // @license MIT
 // @match https://github.com/*
+// @require https://greasyfork.org/scripts/393085-commonsutil/code/CommonsUtil.js?version=754478
 // @grant none
 // @connect github.com
 // @icon https://github.githubassets.com/pinned-octocat.svg
 // ==/UserScript==
 
 (function () {
-  // 删除仓库页的 "Open in Desktop"
-  if (!!document.querySelector('.get-repo-modal-options .mt-2 .tooltipped.js-get-repo')) {
-    document.querySelector('.get-repo-modal-options .mt-2 .tooltipped.js-get-repo').remove();
-  }
-  // 删除文件页的 "Open this file in GitHub Desktop"
-  if (!!document.querySelector(".btn-octicon.tooltipped")) {
-    document.querySelector(".btn-octicon.tooltipped").remove();
+  window.onload = function () {
+    console.log('github-hide-desktop start!');
+    // 删除仓库页的 "Open in Desktop"
+    removeIt('.get-repo-modal-options .mt-2 .tooltipped.js-get-repo', true);
+    // 删除文件页的 "Open this file in GitHub Desktop"
+    removeIt('.btn-octicon.tooltipped', true);
+    console.log('github-hide-desktop end!');
   }
 })();
