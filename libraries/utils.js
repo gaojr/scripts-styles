3f284599c91e1cb84b46a24ed7c76d0db7554989
gaojr <gaojr427@hotmail.com>
2019-12-02 10:01:19 +0800

    update utils.js

--- libraries/utils.js
+++ libraries/utils.js
@@ -3,7 +3,7 @@
 // @namespace https://github.com/gaojr/tampermonkey-scripts
 // @name:CN-zh_cn 工具类
 // @name CommonsUtil
-// @version 0.1
+// @version 0.2
 // @description utility methods
 // @grant none
 // ==/UserScript==
@@ -11,11 +11,52 @@
 /**
  * 输出错误
  * @param functionName 方法名
- * @param selector 选择器
  * @param error 错误
  */
-const error = function (functionName, selector, error) {
-  console.error('function name: ' + functionName + "\nselector: " + selector + "\nerror: " + error);
+const error = function (functionName, error) {
+  console.error('function name: ' + functionName + "\nerror: " + error);
+};
+
+/**
+ * 循环移除元素
+ * @param ele 元素
+ */
+const removeRecursively = function (ele) {
+  try {
+    let parent = ele.parentElement;
+    ele.remove();
+    if (!!parent && !parent.innerHTML) {
+      removeRecursively(parent);
+    }
+  } catch (e) {
+    error('removeRecursively', e);
+  }
+};
+
+/**
+ * 移除选择器对象
+ * @param selector 选择器
+ */
+const removeIt = function (selector) {
+  try {
+    removeRecursively(document.querySelector(selector));
+  } catch (e) {
+    error('removeIt', e);
+  }
+};
+
+/**
+ * 移除选择器所有对象
+ * @param selector 选择器
+ */
+const removeAll = function (selector) {
+  try {
+    document.querySelectorAll(selector).forEach(function (ele) {
+      removeRecursively(ele);
+    });
+  } catch (e) {
+    error('removeAll', e);
+  }
 };
 
 /**
@@ -26,32 +67,6 @@ const clickIt = function (selector) {
   try {
     document.querySelector(selector).click();
   } catch (e) {
-    error('clickIt', selector, e);
-  }
-};
-
-/**
- * 移除选择器对象
- * @param selector 选择器
- */
-const removeIt = function (selector) {
-  try {
-    document.querySelector(selector).remove();
-  } catch (e) {
-    error('removeIt', selector, e);
-  }
-};
-
-/**
- * 移除选择器所有对象
- * @param selector 选择器
- */
-const removeAll = function (selector) {
-  try {
-    document.querySelectorAll(selector).forEach(function (e) {
-      e.remove();
-    });
-  } catch (e) {
-    error('removeAll', selector, e);
+    error('clickIt', e);
   }
 };
